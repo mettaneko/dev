af6fa77d4fae08ae45d3a9b2e77a8361204aca3a
MettaDev <55776002+mettaneko@users.noreply.github.com>
2024-02-19 00:00:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,21 +5,11 @@ const scroll1 = window.scrollY;
 var parent = document.getElementById("body")
 
 if (screenWidth <= 520) {
-  document.getElementById("prog").href = "#programms-mobile";
-  document.getElementById("prochee-button").href = "#prochee-mobile";
   parent.removeChild(document.getElementById("plus"))
-  parent.removeChild(document.getElementById("programms"))
-  parent.removeChild(document.getElementById("prochee"))
 }
 if (screenWidth > 520) {
-  document.getElementById("prog").href = "#programms";
-  document.getElementById("prochee-button").href = "#prochee";
   parent.removeChild(document.getElementById("plus-mobile"))
   parent.removeChild(document.getElementById("plus-mobile"))
   parent.removeChild(document.getElementById("plus-mobile"))
-  parent.removeChild(document.getElementById("programms-mobile"))
-  parent.removeChild(document.getElementById("programms-mobile"))
-  parent.removeChild(document.getElementById("prochee-mobile"))
-  parent.removeChild(document.getElementById("prochee-mobile"))
-  parent.removeChild(document.getElementById("prochee-mobile"))
+  parent.removeChild(document.getElementById("plus-mobile"))
 }
